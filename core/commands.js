60fed986b073d4d8ea6b7a6896ab5a5240769d42
Jay Baker <logikal@gmail.com>
2015-10-30 09:49:29 +1300

    sugarifying the commands class

--- core/commands.js
+++ core/commands.js
@@ -1,83 +1,84 @@
+"use strict";
+
 var commands = [];
 
 var fs = require('fs');
 var path = require('path');
 
 /**
- * Commands object
- * @constructor
+ * Commands class used to by the chat server and its plugins to manage the list of commands
  */
-var Commands = {};
+class Commands {
+  /**
+   * Add a command into the chat servers list
+   * @param {String} name        The name of the command used for invocation
+   * @param {String} description The description of the command used for help messages
+   * @param {Integer} level      The minimum level required to execute the command
+   * @param {Function} exec      The function to be executed
+   */
+  static add(name, description, level, exec) {
+    this.init();
 
-/**
- * Load all the commands from ./commands directory
- */
-Commands.load = function() {
-  var cmdsDir = path.join(__dirname, '../commands');
-  var files = fs.readdirSync(cmdsDir);
+    commands[name] = { name, description, level, exec };
+  }
 
-  process.emit('chat.commands.loading', files);
+  /**
+   * Return all commands
+   * @return {Array} array of commands loaded
+   */
+  static all() {
+    this.init();
 
-  for (var file in files) {
-    try {
-      require(path.join(cmdsDir, files[file]));
-    } catch (e) {
-      console.log("Error loading command '%s':\n %s", files[file], e);
+    return commands;
+  }
+
+  /**
+   * Execute a command
+   * @param  {Client} client  the client that is executing the command
+   * @param  {Array}  command the array of the command payload
+   */
+  static exec(client, command) {
+    this.init();
+
+    var name = command.shift();
+
+    console.log("%s sent command: %s", client.name, name);
+    //console.log(client);
+    if (commands[name] !== undefined) {
+      //arguments.shift();
+      commands[name].exec(client, name, command);
     }
   }
-};
 
-/**
- * Init the commands and load them if not initalised
- */
-Commands.init = function() {
-  // only if commands is undefined do we want to action the init
-  if (typeof commands === undefined) {
-    commands = [];
+  /**
+   * Init the commands and load them if not initalised
+   */
+  static init() {
+    // only if commands is undefined do we want to action the init
+    if (typeof commands === undefined) {
+      commands = [];
 
-    this.load();
+      this.load();
+    }
   }
-};
 
-/**
- * Add a command to the list
- * @param  {String} name        the name of the command being added
- * @param  {String} description the description of the command being added
- * @param  {Integer} level      min level for execution of the command
- * @param  {Function} command   the function that gets executed
- */
-Commands.add = function(name, description, level, command) {
-  this.init();
+  /**
+   * Load all the commands from ./commands directory
+   */
+  static load() {
+    var cmdsDir = path.join(__dirname, '../commands');
+    var files = fs.readdirSync(cmdsDir);
 
-  commands[name] = { name: name, description: description, level: level, exec: command };
-};
+    process.emit('chat.commands.loading', files);
 
-/**
- * Execute a command
- * @param  {Client} client  the client that is executing the command
- * @param  {Array}  command the array of the command payload
- */
-Commands.exec = function(client, command) {
-  this.init();
-
-  var name = command.shift();
-
-  console.log("%s sent command: %s", client.name, name);
-  //console.log(client);
-  if (commands[name] !== undefined) {
-    //arguments.shift();
-    commands[name].exec(client, name, command);
+    for (var file in files) {
+      try {
+        require(path.join(cmdsDir, files[file]));
+      } catch (e) {
+        console.log("Error loading command '%s':\n %s", files[file], e);
+      }
+    }
   }
-};
-
-/**
- * Return all commands
- * @return {Array} array of commands loaded
- */
-Commands.all = function() {
-  this.init();
-
-  return commands;
-};
+}
 
 module.exports = Commands;
