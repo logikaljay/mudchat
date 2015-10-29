4279ee6e6ea2ce045c11c9c8041843391d06a4d4
Jay Baker <logikal@gmail.com>
2015-10-30 10:01:46 +1300

    sugarifying the client class

--- core/client.js
+++ core/client.js
@@ -1,116 +1,108 @@
-var protocol;
-var channel;
-var room;
-var account;
-var messageLog;
-var snoopLog;
-var snoopers = [];
-var snooping = false;
-var myName;
-var address;
-var version;
-var port;
-var authenticated;
-var gagged;
-var gagTime;
-var lastActivity;
-var socket;
-var self;
+"use strict";
 
-/**
- * Construct a new client
- * @constructor
- * @param  {String} name the name of the client
- * @param  {String} ip   the ip address of the client
- * @return {Client}      the client
- */
-var Client = function(name, ip) {
-  this.name = name;
-  this.ip = ip;
-
-  return this;
-};
-
-/**
- * Set the clients socket
- * @param  {Socket} socket the socket the client is connected with
- */
-Client.prototype.setSocket = function(socket) {
-  this.socket = socket;
-
-  // listen for all commands
-  socket.on('data', messageReceived.bind(this));
-};
-
-/**
- * Kill the clients connection
- */
-Client.prototype.kill = function() {
-  //this.socket.destroy();
-};
-
-/**
- * Send a message to the client
- * @param  {String}   message the message to send
- * @param  {Boolean} private if true, send a private message to the client
- */
-Client.prototype.send = function(message, private) {
-  var hexMessage = "";
-  for (var i = 0; i < message.length; i++) {
-    hexMessage += ''+message.charCodeAt(i).toString(16);
+class Client {
+  /**
+   * Construct a new client
+   * @constructor
+   * @param  {String} name the name of the client
+   * @param  {String} ip   the ip address of the client
+   * @return {Client}      the client
+   */
+  constructor(name, ip) {
+    this.name = name;
+    this.ip = ip;
   }
 
-  // send the message
-  var buf = new Buffer((private ? "05" : "04") + hexMessage + "FF", 'hex');
-  this.socket.write(buf);
-};
+  /**
+   * Set the clients socket and sets up the 'data' event listener
+   * @param  {Socket} socket the socket the client is connected with
+   */
+  setSocket(socket) {
+    this.socket = socket;
+    socket.on('data', data => this.messageReceived(data));
+  }
 
-/**
- * Message received from the Client
- * @private
- * @param  {Buffer} data The data that was received
- */
-var messageReceived = function(data) {
-  var str = data.toString();
-  var command = data[0].toString(16);
-  var payload = {
-    client: this,
-    data: data.toString().substring(2, data.length - 2)
-  };
+  /**
+   * Kill the client's connection
+   */
+  kill() {
+    this.socket.destroy();
+  }
 
-  switch (command) {
-    case "13":
-      // some clients send through the version twice (I'm looking at you TinTin++)
-      break;
-    case "4":
-      // message to all = 4: \nTinTin chats to everyone, 'hi'
-      process.emit('chat.client.message.room', payload);
-      break;
-    case "5":
-      // private message = 5: \nTinTin chats to you, 'hi'
-      process.emit('chat.client.message.command', payload);
-      break;
-    case "1a":
-      // ping request = 1a: 1446068720587471
-      break;
-    case "1":
-      // client name change = 1: bob
-      break;
-    case "1c":
-      // peek connections = 1c:
-      break;
-    case "74":
-      // send raw data message = 74: t++
-      break;
-    case "14":
-      // upload file request = 14: tt++,835392
-      break;
-    case "7":
-      // serve'd by client = 7: \nbob is now chat serving you.\n
-      break;
-    case "19":
-      // cancel file request
-      break;
+  /**
+   * Send a message to the client
+   * @param  {String}   message the message to send
+   * @param  {Boolean} isPrivate if true, send a private message to the client
+   */
+  send(message, isPrivate) {
+    var hexMessage = "";
+    for (var i = 0; i < message.length; i++) {
+      hexMessage += ''+message.charCodeAt(i).toString(16);
+    }
+
+    // send the message
+    var buf = new Buffer((isPrivate ? "05" : "04") + hexMessage + "FF", 'hex');
+    this.socket.write(buf);
+  }
+
+  /**
+   * Message received from the Client
+   * @param  {Buffer} data The data that was received
+   */
+  messageReceived(data) {
+    var str = data.toString();
+    var command = data[0].toString(16);
+    var payload = {
+      client: this,
+      data: data.toString().substring(2, data.length - 2)
+    };
+
+    this.handleMessage(command,payload);
+  }
+
+  /**
+   * Handle the message that was received
+   * @param  {String} command The hex byte command received from the client's chat software
+   * @param  {Object} payload The payload contains the client and also the message data stripped of any protocol information
+   */
+  handleMessage(command, payload) {
+    switch (command) {
+      case "13":
+        // some clients send through the version twice (I'm looking at you TinTin++)
+        break;
+      case "4":
+        // message to all = 4: \nTinTin chats to everyone, 'hi'
+        process.emit('chat.client.message.room', payload);
+        break;
+      case "5":
+        // private message = 5: \nTinTin chats to you, 'hi'
+        process.emit('chat.client.message.command', payload);
+        break;
+      case "1a":
+        // ping request = 1a: 1446068720587471
+        break;
+      case "1":
+        // client name change = 1: bob
+        break;
+      case "1c":
+        // peek connections = 1c:
+        break;
+      case "74":
+        // send raw data message = 74: t++
+        break;
+      case "14":
+        // upload file request = 14: tt++,835392
+        break;
+      case "7":
+        // serve'd by client = 7: \nbob is now chat serving you.\n
+        break;
+      case "19":
+        // cancel file request
+        break;
+      default:
+        // unrecognised commands
+        break;
+    }
   }
 }
 
