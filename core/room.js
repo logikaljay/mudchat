76629578c4de43792650387150bdb999c42489cc
Jay Baker <logikal@gmail.com>
2015-10-30 09:53:48 +1300

    sugarifying the room class

--- core/room.js
+++ core/room.js
@@ -1,3 +1,5 @@
+"use strict";
+
 var name;
 var password;
 var destroyable;
@@ -11,116 +13,116 @@ var util = require('util');
 var Server = require('./server');
 
 /**
- * Room constructor
- * @constructor
- * @param  {String} roomName  name of the room
- * @param  {String} password  password for the room
- * @param  {Integer} minLevel minimum level for the room
- * @return {Room}             the room object
+ * The room class handles the creation, joining, leaving and all messages posted to rooms
  */
-var Room = function(roomName, password, minLevel) {
-  this.name = roomName;
-  this.password = password;
-  this.minLevel = minLevel;
+class Room {
+  /**
+   * Room constructor
+   * @constructor
+   * @param  {String} roomName  name of the room
+   * @param  {String} password  password for the room
+   * @param  {Integer} minLevel minimum level for the room
+   * @return {Room}             the room object
+   */
+  constructor(roomName, password, minLevel) {
+    this.name = roomName;
+    this.password = password;
+    this.minLevel = minLevel;
 
-  this.destroyable = true;
-  this.silent = false;
+    this.destroyable = true;
+    this.silent = false;
 
-  this.clients = [];
-  this.listeners = [];
+    this.clients = [];
+    this.listeners = [];
 
-  if (this.name !== "main") {
-    var lvlString = minLevel > 0 ? "(minLvl "+this.minLvl+")" : "";
+    if (this.name !== "main") {
+      var lvlString = minLevel > 0 ? "(minLvl "+this.minLvl+")" : "";
+    }
+
+    process.emit('chat.room.create', this);
+    process.on('chat.client.message.room', this.send.bind(this));
   }
 
-  process.emit('chat.room.create', this);
-  process.on('chat.client.message.room', this.send.bind(this));
+  destroy() {
+    // get the chat server
+    var self = this;
+    var Server = require('./server');
+    var rooms = Server.getInstance().rooms;
 
-  return this;
-};
+    // remove this room from the list of rooms
+    rooms = rooms.filter(function(room) {
+      console.log("Removing '" + room.name + "': " + (room.name == self.name));
+      return room.name !== self.name;
+    });
+    Server.getInstance().rooms = rooms;
 
-/**
- * Destroy a room
- */
-Room.prototype.destroy = function() {
-  // get the chat server
-  var self = this;
-  var Server = require('./server');
-  var rooms = Server.getInstance().rooms;
+    // because this is no longer referenced, it should get GC'd
+  }
 
-  // remove this room from the list of rooms
-  rooms = rooms.filter(function(room) {
-    console.log("Removing '" + room.name + "': " + (room.name == self.name));
-    return room.name !== self.name;
-  });
-  Server.getInstance().rooms = rooms;
+  /**
+   * Send a message to all clients in a room
+   * @param  {Object} payload the message to send, and the client it was sent from
+   */
+  send(payload) {
+    for (var i in this.clients) {
+      var client = this.clients[i];
 
-  // because this is no longer referenced, it should get GC'd
-};
+      // console.log("room %s contains %s", this.name, this.clients.length);
 
-/**
- * Send a message to all clients in a room
- * @param  {Object} payload the message to send, and the client it was sent from
- */
-Room.prototype.send = function(payload) {
-  for (var i in this.clients) {
-    var client = this.clients[i];
-
-    // console.log("room %s contains %s", this.name, this.clients.length);
-
-    if (payload.client === null) {
-      client.send(payload.data, false);
-    } else if (client.name !== payload.client.name) {
-      client.send(payload.data, false);
+      if (payload.client === null) {
+        client.send(payload.data, false);
+      } else if (client.name !== payload.client.name) {
+        client.send(payload.data, false);
+      }
     }
   }
-};
 
-/**
- * Join a room
- * @param  {Client}  client the client to put in the room
- * @param  {Boolean} silent if true, don't advertise it to the room
- */
-Room.prototype.join = function(client, silent) {
-  // add the client to this room's list of clients
-  this.clients.push(client);
+  /**
+   * Join a room
+   * @param  {Client}  client the client to put in the room
+   * @param  {Boolean} silent if true, don't advertise it to the room
+   */
+  join(client, silent) {
+    // add the client to this room's list of clients
+    this.clients.push(client);
 
-  // set the client's room
-  client.room = this;
+    // set the client's room
+    client.room = this;
 
-  if (silent === undefined) {
-    this.send({ client: client, data: util.format('%s has joined the room', client.name) });
+    if (silent === undefined) {
+      this.send({ client: client, data: util.format('%s has joined the room', client.name) });
+    }
+
+    client.send(util.format("You have joined the '%s' room", this.name));
   }
 
-  client.send(util.format("You have joined the '%s' room", this.name));
-};
+  /**
+   * Leave a room
+   * @param  {Object}  client the client to leave the room
+   * @param  {Boolean} silent if true, don't advertise it to the room
+   */
+  leave(client, silent) {
+    // remove the client from this room's list of clients
+    this.clients = this.clients.filter(function(c) {
+      return client.name !== c.name;
+    });
 
-/**
- * Leave a room
- * @param  {Object}  client the client to leave the room
- * @param  {Boolean} silent if true, don't advertise it to the room
- */
-Room.prototype.leave = function(client, silent) {
-  // remove the client from this room's list of clients
-  this.clients = this.clients.filter(function(c) {
-    return client.name !== c.name;
-  });
+    if (silent === undefined) {
+      this.send({ client: client, data: util.format('%s has left the room', client.name) });
+    }
 
-  if (silent === undefined) {
-    this.send({ client: client, data: util.format('%s has left the room', client.name) });
+    // No need to tell the user they have left the room - joining seems good enough.
+    // client.send(util.format("You have left the '%s' room", this.name));
   }
 
-  // No need to tell the user they have left the room - joining seems good enough.
-  // client.send(util.format("You have left the '%s' room", this.name));
-};
-
-/**
- * Get the room
- * @param  {String} name  the name of the room to get
- * @return {Room}         the room object
- */
-Room.prototype.get = function(name) {
-  return Server.getInstance().rooms[name];
-};
+  /**
+   * Get the room
+   * @param  {String} name  the name of the room to get
+   * @return {Room}         the room object
+   */
+  get(name) {
+    return Server.getInstance().rooms[name];
+  }
+}
 
 module.exports = Room;
