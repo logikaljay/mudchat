0ee15766393f7b0ab6d453850b307772a2922424
Jay Baker <logikal@gmail.com>
2015-10-30 15:20:18 +1300

    adding color to core

---
+++ core/color.js
@@ -0,0 +1,40 @@
+"use strict";
+
+const ANSIColor = {
+  NRM: chr(27) + "[0m",
+  BLD: chr(27)+ "[1m",
+	DAR: chr(27) + "[2m",
+	UND: chr(27) + "[4m",
+	BLN: chr(27) + "[5m",
+	REV: chr(27) + "[7m",
+	BLK: chr(27) + "[30m",
+	RED: chr(27) + "[31m",
+	GRN: chr(27) + "[32m",
+	YEL: chr(27) + "[33m",
+	BLU: chr(27) + "[34m",
+	MAG: chr(27) + "[35m",
+	CYN: chr(27) + "[36m",
+	WHT: chr(27) + "[37m"
+};
+
+function chr(num) {
+  //  discuss at: http://phpjs.org/functions/chr/
+  // original by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)
+  // improved by: Brett Zamir (http://brett-zamir.me)
+  //   example 1: chr(75) === 'K';
+  //   example 1: chr(65536) === '\uD800\uDC00';
+  //   returns 1: true
+  //   returns 1: true
+
+  if (num > 0xFFFF) { // Create a four-byte string (length 2) since this code point is high
+    //   enough for the UTF-16 encoding (JavaScript internal use), to
+    //   require representation with two surrogates (reserved non-characters
+    //   used for building other characters; the first is "high" and the next "low")
+    num -= 0x10000;
+    return String.fromCharCode(0xD800 + (num >> 10), 0xDC00 + (num & 0x3FF));
+  }
+
+  return String.fromCharCode(num);
+}
+
+module.exports = ANSIColor;
