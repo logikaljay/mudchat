4ce6bdf08ac49151fddca8b15e6d08da8fe09618
Jay Baker <logikal@gmail.com>
2015-11-21 12:13:38 +1300

    refactor(Plugin): cleaning up the sample plugin

--- src/plugins/hi.js
+++ src/plugins/hi.js
@@ -4,20 +4,20 @@ var util = require('util');
 
 var MessageEvent = require('../core/messageevent');
 var Plugin = require('../core/plugin');
-var Server = require('../core/server');
+var Room = require('../core/room');
 var Color = require('../core/color');
 
 var name;
 
 class Hi extends Plugin {
   onLoad() {
-    var room = Server.getInstance().rooms.get('main');
+    var room = Room.get('main');
     var message = util.format('%s %s', Color.header('hi'), Color.details('plugin', 'loaded'));
     MessageEvent.private(message).toClients(room.clients).send();
   }
 
   onUnload() {
-    var room = Server.getInstance().rooms.get('main');
+    var room = Room.get('main');
     var message = util.format('%s %s', Color.header('hi'), Color.details('plugin', 'unloaded'));
     MessageEvent.private(message).toClients(room.clients).send();
   }
